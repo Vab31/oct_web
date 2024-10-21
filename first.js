2c2bea27b2bb27bef587d1af1a5415f66370c23e
Vaibhav Singh <singhvaibhav654@gmail.com>
2024-10-21 19:33:24 +0530

    Hello World!

---
+++ first.js
@@ -0,0 +1,202 @@
+// let  
+// var
+// const
+
+
+// {
+
+
+// // let u=9;
+
+
+// }
+
+
+// var y = 0;
+// var y =8;
+
+
+
+
+
+var i=3;
+
+// var ui= u + i;
+// var ui= u - i;
+// var ui= u * i;
+// var ui= u / i;
+// var ui= u % i;
+// var ui= u ** i;
+// var u=9;
+// console.log(u++) //10
+// console.log(--u); //9
+// console.log(++u);  //10
+// console.log(u)
+
+// ||
+// &&
+
+
+// console.log(true || false)
+// console.log(true || true)
+// console.log(false || true)
+// console.log(false || false)
+
+// console.log(true && false || false)
+// console.log( || false)
+// console.log(false || true && true)
+// console.log(false || false && true)
+
+// var u= 'dfsfsffs fsdfd'
+// var u = 5545;
+// var u1 = "5545";
+
+// console.log(u==u1)
+// console.log(u===u1)
+
+
+// let op;
+
+// let ui=null
+
+// console.log(op)
+
+// console.log(ui);
+
+// console.log(ui==op)
+// console.log(ui===op)
+
+var age = 18
+var gender = "female"
+  
+// if(gender==female &&age>=18){
+//      console.log("eligible for voting")
+//    }
+// else {
+// console.log("not eligible for voting ")
+// }
+
+
+
+
+
+// var age = 18
+// var gender = "female"
+  
+// if(gender==female &&age>=18){
+//      console.log("eligible for voting")
+//    }
+// else {
+// console.log("not eligible for voting ")
+// }
+
+// let u1="ui";
+// let u2="op"
+// let member="jk"
+
+
+
+// if(member==u1 || member ==u2 ){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+// if(member ==u1){
+//     console.log(true);
+// }else if(member == u2){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+let op = [1,5,8,6,7];
+
+// op[2]=10;
+
+// console.log(op[2]);
+
+
+let y2="dsfddfs";
+
+// y2[2]="4"
+
+// console.log(y2[2]);
+
+
+// let op1 = [[1,2,3],
+//            [4,5,6],
+//            [7,8,9]];
+
+// console.log(op1[0][1])
+
+// console.log("helo")
+// console.log("helo")
+// console.log("helo")
+
+// for(i=0;i<6;i++){
+
+// if(i==3){
+//     continue;
+// }
+
+//     console.log(i)
+// }
+
+let i1=0;
+
+// while(i1<4){
+
+//     console.log(i1)
+
+// i1++;
+// }
+
+// while(false){
+//     console.log("hello")
+// }
+
+
+// do{
+
+//     console.log("hello")
+
+// }while(false)
+
+    
+
+
+
+// function ui(o,p){
+
+// console.log("hello")
+
+// console.log(o);
+// console.log(p)
+
+// return o+p
+
+// }
+
+
+// let jk=ui(9,6);
+
+
+// console.log(jk)
+
+
+function average(a,b,c,d,e){
+    
+    // Console.log(average)
+let ui = a+b+c+d+e/5
+
+return ui
+
+     }
+
+let jk=average(98,86,92,74,88)
+
+console.log(jk)
+ 
